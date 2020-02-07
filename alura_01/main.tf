8dd5166d6a588135b4fbd212f529000ff526f47e
anderson.satana <anderson.santana@avecbrasil.com.br>
2020-02-07 01:17:30 -0300

    alura-curso-terraform

---
+++ alura_01/main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  version = "~> 2.0"
+  region     = "${var.region}"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+}
+
+resource "aws_instance" "dev" {
+  count = 3
+  ami = "ami-026c8acd92718196b"
+  instance_type = "t2.micro"
+  key_name = "terraform-aws"
+  tags = {
+    Name = "dev${count.index}"
+  }
+  vpc_security_group_ids = ["sg-02e0bc4590ba7731e"]
+}
+
+resource "aws_security_group" "acesso-ssh" {
+  name        = "acesso-ssh"
+  description = "acesso-ssh"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = ["${var.my_ip}"]
+  }
+  tags = {
+    Name = "ssh"
+  }
+}
