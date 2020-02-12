47447e7698f0a283f61b6c8f62a7e6eb89af6747
anderson.satana <anderson.santana@avecbrasil.com.br>
2020-02-11 22:56:24 -0300

    aulas alura

--- alura_01/main.tf
+++ alura_01/main.tf
@@ -5,6 +5,14 @@ provider "aws" {
   secret_key = "${var.secret_key}"
 }
 
+provider "aws" {
+  alias = "us-east-2"
+  version = "~> 2.0"
+  region     = "us-east-2"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+}
+
 resource "aws_instance" "dev" {
   count = 3
   ami = "ami-026c8acd92718196b"
@@ -13,22 +21,46 @@ resource "aws_instance" "dev" {
   tags = {
     Name = "dev${count.index}"
   }
-  vpc_security_group_ids = ["sg-02e0bc4590ba7731e"]
+  vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
 }
 
-resource "aws_security_group" "acesso-ssh" {
-  name        = "acesso-ssh"
-  description = "acesso-ssh"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    # Please restrict your ingress to only necessary IPs and ports.
-    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
-    cidr_blocks = ["${var.my_ip}"]
-  }
+resource "aws_instance" "dev4" {
+  ami = "ami-026c8acd92718196b"
+  instance_type = "t2.micro"
+  key_name = "terraform-aws"
   tags = {
-    Name = "ssh"
+    Name = "dev4"
+  }
+  vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
+  depends_on = ["aws_s3_bucket.dev4"]
+}
+
+resource "aws_instance" "dev5" {
+  provider = "aws.us-east-2"
+  ami = "ami-02ccb28830b645a41"
+  instance_type = "t2.micro"
+  key_name = "terraform-aws"
+  tags = {
+    Name = "dev5"
+  }
+  vpc_security_group_ids = ["${aws_security_group.acesso-ssh-us-east-2.id}"]
+  depends_on = ["aws_dynamodb_table.dynamodb-homologacao"]
+}
+
+resource "aws_dynamodb_table" "dynamodb-homologacao" {
+  provider = "aws.us-east-2"
+  name           = "GameScores"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "UserId"
+  range_key      = "GameTitle"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  attribute {
+    name = "GameTitle"
+    type = "S"
   }
 }
